000b7768cbc7ff4c4cfe3f3ce4da77f440b4d9f0
Tigist Wondimneh <wondimnehtigist@gmail.com>
2023-03-27 16:50:52 +0300

    add comment

--- content.py
+++ content.py
@@ -1,7 +1,10 @@
 # snippet - 1
+# Calculates area of a circle
 def calculateArea(radius):
     return 3.14 * radius ** 2
 
+
+# Calculates the quotient of two numbers
 def divide_numbers(a, b):
     return a / b
 
