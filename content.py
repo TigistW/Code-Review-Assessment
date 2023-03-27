f68f63adb9f578f2f3f2ab889f29a17825333a72
Tigist Wondimneh <wondimnehtigist@gmail.com>
2023-03-27 17:23:39 +0300

    add comment

--- content.py
+++ content.py
@@ -12,3 +12,27 @@ def divide_numbers(a, b):
         return
     return a / b
 
+# snippets-2
+
+# validates an iterable whether it is a valid parenthesis sequence or not
+def is_valid_parenthesis(s):
+    # define a dictionary that defines the valid parenthesis elements and answer space
+    dc = {
+        '(': ')',
+          "{": "}",
+         '[': ']'
+    }
+    stack = []
+    
+    for i in s:
+        if i in dc.keys(): stack.append(i)
+        else:
+            if len(stack) == 0:
+                return False
+            
+            op = stack.pop()
+            if dc[op] != i:
+                return False
+    if len(stack) != 0:
+        return False
+    return True
