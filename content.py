252f6668d0a742b65cf670dde03974e4b2925983
EyasuTesfu <jt272525@gmail.com>
2023-03-27 17:12:29 +0300

    add snippet 2 to content.py

--- content.py
+++ content.py
@@ -2,6 +2,32 @@
 def calculateArea(radius):
     return 3.14 * radius ** 2
 
+
 def divide_numbers(a, b):
     return a / b
 
+# snippet - 2
+
+
+def is_valid_parenthesis(s):
+    dc = {
+        '(': ')',
+        "{": "}",
+        '[': ']'
+
+    }
+    stack = []
+    for i in s:
+        if i in dc.keys():
+            stack.append(i)
+        else:
+            if len(stack) == 0:
+                return False
+
+            op = stack.pop()
+            if dc[op] != i:
+                return False
+    if len(stack) != 0:
+        return False
+
+    return True
