7f20e78123723a95d33fd2d7f7b7eb174e0c6010
Tigist Wondimneh <wondimnehtigist@gmail.com>
2023-03-27 17:01:37 +0300

    add comment and error handling

--- content.py
+++ content.py
@@ -1,10 +1,14 @@
 # snippet - 1
 # Calculates area of a circle
+import math
 def calculateArea(radius):
-    return 3.14 * radius ** 2
+    return math.pi * (radius ** 2)
 
 
 # Calculates the quotient of two numbers
 def divide_numbers(a, b):
+    if b == 0:
+        print("Cannot divide by zero!")
+        return
     return a / b
 
